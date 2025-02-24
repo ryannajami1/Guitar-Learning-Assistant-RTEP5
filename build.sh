6b3ee8c07bb0f6b774559ca00dbc83aeaf88ff1e
Finlay Durkin <finlaydurkin@gmail.com>
2025-02-24 14:15:19 +0000

    updated readme and build file

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ mkdir -p build
 cd build
 
 # build the project
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $1 ..
+cmake -G "Unix Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON $1 ..
 make
 
 # Change back to the original directory
