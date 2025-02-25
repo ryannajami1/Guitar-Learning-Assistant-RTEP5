eb1bf52750dc2ede42671eafad7dba41a0d49772
Finlay Durkin <finlaydurkin@gmail.com>
2025-02-25 00:25:08 +0000

    peak detection algorithm added, surrounding code broken

--- src/chord-detection/ChordDetection.cpp
+++ src/chord-detection/ChordDetection.cpp
@@ -1,82 +1,123 @@
 #include <cstdio>
 #include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <array>
+#include <cmath>
 
 using namespace std;
 
-vector<int> detect_peaks(const vector<float> &data, float threshold, float min_peak_width, float min_peak_height) {
-    
-    // Find peaks and valleys then filter based on distance
-    
-    vector<int> peak_indexes;
+class ChordDetection {
+public:
+    enum State {
+        UNKNOWN,
+        FIND_PEAK,
+        FIND_TROUGH
+    };
 
-    float current_value;
-    float prev_value;
+    vector<int> determine_peaks(const vector<float> &data, float threshold, float min_height, float min_width) {
+        float mask_top = data[0];
+        float mask_bottom = mask_top - min_height;
 
-    float prev_highest = data[0];
-    int prev_highest_index = 0;
+        float highest = data[0];
+        int highest_index = 0;
+        float lowest = data[0];
 
-    float prev_lowest = data[0];
-    int prev_lowest_index = 0;
+        vector<int> peak_indices;
 
-    bool ascending = false;
+        State state = UNKNOWN;
 
-    for (size_t i = 1; i < data.size() - 1; i++) {
-        current_value = data[i];
-
-        // Push the boundaries
-
-        if (current_value > prev_highest) {
-            prev_highest = current_value;
-            prev_highest_index = i;
-        }
-        else if (current_value < prev_lowest) {
-            prev_lowest = current_value;
-        }
-
-        // Check for peaks and troughs
-
-        if ((current_value - prev_lowest) > min_peak_height) {
-            // Prev lowest was a valid trough. We are now ascending
-
-            if (!ascending) {
-                ascending = true;
-
-                // Reset the highest value to find new peak
-                prev_highest = current_value;
-                prev_highest_index = i;
+        for (size_t i = 0; i < data.size(); i++) {
+            // move mask
+            if (data[i] > mask_top) {
+                mask_top = data[i];
+                mask_bottom = mask_top - min_height;
+                highest = data[i];
+                highest_index = i;
+            } else if (data[i] < mask_bottom) {
+                mask_bottom = data[i];
+                mask_top = mask_bottom + min_height;
+                lowest = data[i];
             }
 
-            // printf("i: %zu\nmax: %f\ncur: %f\n", i, prev_highest, current_value);
-        }
-
-        if ((prev_highest - current_value) > min_peak_height) {
-            // Prev highest was a valid peak. We are now descending
-            if (ascending) {
-                ascending = false;
-
-                if (prev_highest > threshold) {
-                    peak_indexes.push_back(prev_highest_index);
+            // Check if mask has shifted away from peak or trough
+            if (highest > mask_top) {
+                if (state == UNKNOWN || state == FIND_PEAK) {
+                    state = FIND_TROUGH;
+                    if (highest > threshold) {
+                        peak_indices.push_back(highest_index);
+                    }
+                    lowest = mask_bottom;
                 }
-
-                // Reset lowest value to find new trough
-                prev_lowest = current_value;
-                prev_lowest_index = i;
             }
-            // printf("i: %zu\nmin: %f\ncur: %f\n", i, prev_lowest, current_value);
 
+            if (lowest < mask_bottom) {
+                if (state == UNKNOWN || state == FIND_TROUGH) {
+                    state = FIND_PEAK;
+                    highest = mask_top;
+                    highest_index = i;
+                }
+            }
         }
+
+        return peak_indices;
     }
 
-    return peak_indexes;
-}
+    float CalculateNoiseFloor(const vector<float> &data) {
+        float sum = 0.0;
+        for (float value : data) {
+            sum += value;
+        }
+        return sum / data.size();
+    }
 
+    string NoteLookup(float frequency) {
+        vector<string> notes = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+        int note_index = static_cast<int>(round(12 * log2(frequency / 440.0))) % 12;
+        if (note_index < 0) {
+            note_index += 12;
+        }
+        return notes[note_index];
+    }
+};
 
 int main() {
+    ChordDetection chordDetection;
+    // vector<float> data = {0,1,2,3,4,5,4,3,2,7,8,9,6,5,4,8,7,6,5,4,5,6,7,0};
 
-    // vector<float> data = {0.0, 1, 2, 3, 4, 3, 2, 1, 0, 1, 2, 3, 4, 3, 2, 1, 0};
-    vector<float> data = {0,1,2,3,4,5,4,3,2,7,8,9,6,5,4,8,7,6,5,4,5,6,7,0,1};
+    string filename = "scope_4_fft.csv";
+    ifstream file(filename);
+    string line;
 
-    vector<int> indexes = detect_peaks(data, 6, 2, 2);
+    vector<array<float, 2>> fft;
+
+    // discard header
+    getline(file, line);
+
+    // read in data
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string value;
+        array<float, 2> row;
+        if (getline(ss, value, ',')) {
+            row[0] = stof(value);
+        }
+        if (getline(ss, value, ',')) {
+            row[1] = stof(value);
+        }
+        fft.push_back(row);
+    }
+
+    // Get the magnitude data
+    vector<float> fft_magnitude;
+    for (const auto& row : fft) {
+        fft_magnitude.push_back(row[0]);
+    }
+
+    float threshold = chordDetection.CalculateNoiseFloor(fft_magnitude) * 30;
+
+    vector<int> indexes = chordDetection.determine_peaks(fft_magnitude,threshold, 0.1, 0);
 
     for (int index : indexes) {
         printf("%d ", index);
