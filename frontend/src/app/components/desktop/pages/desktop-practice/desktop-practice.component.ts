2e5b3c8fc5740c9949116f656963786ff862a81d
Raphael Nekam <raphael.nekam@aon.at>
2025-02-26 10:21:40 +0000

    Implement countdown bar for practice
    Closes #16

--- frontend/src/app/components/desktop/pages/desktop-practice/desktop-practice.component.ts
+++ frontend/src/app/components/desktop/pages/desktop-practice/desktop-practice.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ChordSchematicComponent} from '../../../widgets/chord-schematic/chord-schematic.component';
+import {TimerComponent} from '../../../widgets/timer/timer.component';
 
 @Component({
   selector: 'app-desktop-practice',
   standalone: true,
-  imports: [],
+  imports: [ChordSchematicComponent, TimerComponent],
   templateUrl: './desktop-practice.component.html',
   styleUrl: './desktop-practice.component.scss'
 })
---
+++ frontend/src/app/components/widgets/chord-schematic/chord-schematic.component.ts
@@ -0,0 +1,36 @@
+import {Component} from '@angular/core';
+
+declare var Raphael: any;
+
+@Component({
+  selector: 'app-chord-schematic',
+  standalone: true,
+  imports: [],
+  templateUrl: './chord-schematic.component.html',
+  styleUrl: './chord-schematic.component.scss'
+})
+export class ChordSchematicComponent {
+
+  private chordContainer: any;
+
+  ngOnInit(): void {
+    this.chordContainer = Raphael.chord('chord-container', this.getRandomGuitarChord());
+    this.chordContainer.element.setSize(200, 200);
+  }
+
+  getRandomGuitarChord(): string {
+    const notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    const chordTypes = ['', 'maj', 'min', '7', 'maj7', 'min7', 'sus4', 'dim', 'aug'];
+
+    const randomNote = notes[Math.floor(Math.random() * notes.length)];
+    const randomChordType = chordTypes[Math.floor(Math.random() * chordTypes.length)];
+
+    return `${randomNote} ${randomChordType}`;
+  }
+
+  reloadChord(): void {
+    this.chordContainer.remove();
+    this.chordContainer = Raphael.chord('chord-container', this.getRandomGuitarChord());
+    this.chordContainer.element.setSize(200, 200);
+  }
+}
