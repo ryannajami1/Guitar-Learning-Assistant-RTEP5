f9090d014d3a2597c89dafd336980e272ff46550
Raphael Nekam <raphael.nekam@aon.at>
2025-02-26 13:46:06 +0000

    Implement settings for chords
    Closes #15

--- frontend/src/app/components/desktop/pages/desktop-practice/desktop-practice.component.ts
+++ frontend/src/app/components/desktop/pages/desktop-practice/desktop-practice.component.ts
@@ -7,11 +7,12 @@ import {LastFiveChordsComponent} from '../../../widgets/last-five-chords/last-fi
 import {AccuracyComponent} from '../../../widgets/accuracy/accuracy.component';
 import {ToggleSwitchModule} from 'primeng/toggleswitch';
 import {FormsModule} from '@angular/forms';
+import {DialogModule} from 'primeng/dialog';
 
 @Component({
   selector: 'app-desktop-practice',
   standalone: true,
-  imports: [ChordSchematicComponent, TimerComponent, ButtonModule, ResponseTimeComponent, LastFiveChordsComponent, AccuracyComponent, ToggleSwitchModule, FormsModule],
+  imports: [DialogModule, ChordSchematicComponent, TimerComponent, ButtonModule, ResponseTimeComponent, LastFiveChordsComponent, AccuracyComponent, ToggleSwitchModule, FormsModule],
   templateUrl: './desktop-practice.component.html',
   styleUrl: './desktop-practice.component.scss'
 })
@@ -19,6 +20,26 @@ export class DesktopPracticeComponent {
 
   currentChord: string;
   showChord: boolean = true;
+  showSettings: boolean = false;
+
+  playA: boolean = true;
+  playB: boolean = true;
+  playC: boolean = true;
+  playD: boolean = true;
+  playE: boolean = true;
+  playF: boolean = true;
+  playG: boolean = true;
+
+  playStandard: boolean = true;
+  playMaj: boolean = true;
+  playMin: boolean = true;
+  play7: boolean = true;
+  playMaj7: boolean = true;
+  playMin7: boolean = true;
+  playSus4: boolean = true;
+  playDim: boolean = true;
+  playAug: boolean = true;
+
 
   constructor() {
     this.currentChord = this.getRandomGuitarChord();
@@ -29,8 +50,61 @@ export class DesktopPracticeComponent {
   }
 
   getRandomGuitarChord(): string {
-    const notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-    const chordTypes = ['', 'maj', 'min', '7', 'maj7', 'min7', 'sus4', 'dim', 'aug'];
+
+    let notes = [];
+
+    if (this.playA) {
+      notes.push('A');
+    }
+    if (this.playB) {
+      notes.push('B');
+    }
+    if (this.playC) {
+      notes.push('C');
+    }
+    if (this.playD) {
+      notes.push('D');
+    }
+    if (this.playE) {
+      notes.push('E');
+    }
+    if (this.playF) {
+      notes.push('F');
+    }
+    if (this.playG) {
+      notes.push('G');
+    }
+
+    let chordTypes = [];
+
+    if (this.playStandard) {
+      chordTypes.push('');
+    }
+    if (this.playMaj) {
+      chordTypes.push('maj');
+    }
+    if (this.playMin) {
+      chordTypes.push('min');
+    }
+    if (this.play7) {
+      chordTypes.push('7');
+    }
+    if (this.playMaj7) {
+      chordTypes.push('maj7');
+    }
+    if (this.playMin7) {
+      chordTypes.push('min7');
+    }
+    if (this.playSus4) {
+      chordTypes.push('sus4');
+    }
+    if (this.playDim) {
+      chordTypes.push('dim');
+    }
+    if (this.playAug) {
+      chordTypes.push('aug');
+    }
+
 
     const randomNote = notes[Math.floor(Math.random() * notes.length)];
     const randomChordType = chordTypes[Math.floor(Math.random() * chordTypes.length)];
