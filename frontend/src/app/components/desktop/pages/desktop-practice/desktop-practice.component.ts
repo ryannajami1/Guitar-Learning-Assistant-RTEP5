b83a689192e3e64b9d30c5fa4455a1ec9250228e
Raphael Nekam <raphael.nekam@aon.at>
2025-03-29 23:15:08 +0000

    Refactor

--- frontend/src/app/components/desktop/pages/desktop-practice/desktop-practice.component.ts
+++ frontend/src/app/components/desktop/pages/desktop-practice/desktop-practice.component.ts
@@ -12,6 +12,7 @@ import {CorrectChordComponent} from '../../../widgets/correct-chord/correct-chor
 import {WrongChordComponent} from '../../../widgets/wrong-chord/wrong-chord.component';
 import {TimeoutComponent} from '../../../widgets/timeout/timeout.component';
 import {ChordsService} from '../../../../services/chords.service';
+import {EChordStatus} from '../../../../models/chord-status.enum';
 
 @Component({
   selector: 'app-desktop-practice',
@@ -21,102 +22,12 @@ import {ChordsService} from '../../../../services/chords.service';
   styleUrl: './desktop-practice.component.scss'
 })
 export class DesktopPracticeComponent {
+  EChordStatus = EChordStatus;
 
-  currentChord: string;
   showChord: boolean = true;
   showSettings: boolean = false;
-  showCorrectChordPlayed: boolean = false;
-  showWrongChordPlayed: boolean = false;
-  showTimeout: boolean = false;
-
-  playA: boolean = true;
-  playB: boolean = true;
-  playC: boolean = true;
-  playD: boolean = true;
-  playE: boolean = true;
-  playF: boolean = true;
-  playG: boolean = true;
-
-  playStandard: boolean = true;
-  playMaj: boolean = true;
-  playMin: boolean = true;
-  play7: boolean = true;
-  playMaj7: boolean = true;
-  playMin7: boolean = true;
-  playSus4: boolean = true;
-  playDim: boolean = true;
-  playAug: boolean = true;
-
 
   constructor(public chordPlayedService: ChordsService) {
-    this.currentChord = this.getRandomGuitarChord();
+    this.chordPlayedService.currentChordToBePlayed = this.chordPlayedService.getRandomGuitarChord();
   }
-
-  timeOver(): void {
-    this.currentChord = this.getRandomGuitarChord();
-  }
-
-  getRandomGuitarChord(): string {
-
-    let notes = [];
-
-    if (this.playA) {
-      notes.push('A');
-    }
-    if (this.playB) {
-      notes.push('B');
-    }
-    if (this.playC) {
-      notes.push('C');
-    }
-    if (this.playD) {
-      notes.push('D');
-    }
-    if (this.playE) {
-      notes.push('E');
-    }
-    if (this.playF) {
-      notes.push('F');
-    }
-    if (this.playG) {
-      notes.push('G');
-    }
-
-    let chordTypes = [];
-
-    if (this.playStandard) {
-      chordTypes.push('');
-    }
-    if (this.playMaj) {
-      chordTypes.push('maj');
-    }
-    if (this.playMin) {
-      chordTypes.push('min');
-    }
-    if (this.play7) {
-      chordTypes.push('7');
-    }
-    if (this.playMaj7) {
-      chordTypes.push('maj7');
-    }
-    if (this.playMin7) {
-      chordTypes.push('min7');
-    }
-    if (this.playSus4) {
-      chordTypes.push('sus4');
-    }
-    if (this.playDim) {
-      chordTypes.push('dim');
-    }
-    if (this.playAug) {
-      chordTypes.push('aug');
-    }
-
-
-    const randomNote = notes[Math.floor(Math.random() * notes.length)];
-    const randomChordType = chordTypes[Math.floor(Math.random() * chordTypes.length)];
-
-    return `${randomNote} ${randomChordType}`;
-  }
-
 }
--- frontend/src/app/services/chords.service.ts
+++ frontend/src/app/services/chords.service.ts
@@ -9,6 +9,26 @@ export class ChordsService {
   constructor() {
   }
 
+  currentChordToBePlayed: string = '';
+
+  chordGenerationIncludeA: boolean = true;
+  chordGenerationIncludeB: boolean = true;
+  chordGenerationIncludeC: boolean = true;
+  chordGenerationIncludeD: boolean = true;
+  chordGenerationIncludeE: boolean = true;
+  chordGenerationIncludeF: boolean = true;
+  chordGenerationIncludeG: boolean = true;
+
+  chordGenerationIncludeStandard: boolean = true;
+  chordGenerationIncludeMaj: boolean = true;
+  chordGenerationIncludeMin: boolean = true;
+  chordGenerationInclude7: boolean = true;
+  chordGenerationIncludeMaj7: boolean = true;
+  chordGenerationIncludeMin7: boolean = true;
+  chordGenerationIncludeSus4: boolean = true;
+  chordGenerationIncludeDim: boolean = true;
+  chordGenerationIncludeAug: boolean = true;
+
   lastChords: EChordStatus[] = [];
 
   showCorrectChordModal: boolean = false;
@@ -36,33 +56,84 @@ export class ChordsService {
     }
   }
 
-  timeoutReached(): void {
-    this.showTimeoutModal = true;
-    this.lastChords.push(EChordStatus.Timeout)
-    this.startResetModalTimer();
-    console.log(this.lastChords)
-  }
-
-  correctChordPlayed(): void {
-    this.showCorrectChordModal = true;
-    this.lastChords.push(EChordStatus.Correct)
-    this.startResetModalTimer();
-    console.log(this.lastChords)
-  }
-
-  wrongChordPlayed(): void {
-    this.showWrongChordModal = true;
-    this.lastChords.push(EChordStatus.Wrong)
-    this.startResetModalTimer();
-    console.log(this.lastChords)
-  }
-
-  startResetModalTimer(): void {
+  chordEventHandler(status: EChordStatus): void {
+    if (status === EChordStatus.Timeout) {
+      this.showTimeoutModal = true;
+      this.lastChords.push(EChordStatus.Timeout)
+    } else if (status === EChordStatus.Correct) {
+      this.showCorrectChordModal = true;
+      this.lastChords.push(EChordStatus.Correct)
+    } else if (status === EChordStatus.Wrong) {
+      this.showWrongChordModal = true;
+      this.lastChords.push(EChordStatus.Wrong)
+    }
     setTimeout(() => {
       this.showCorrectChordModal = false;
       this.showTimeoutModal = false;
       this.showWrongChordModal = false;
+      this.currentChordToBePlayed = this.getRandomGuitarChord();
     }, 1000); // 1000 milliseconds = 1 second
   }
 
+  getRandomGuitarChord(): string {
+    let notes = [];
+
+    if (this.chordGenerationIncludeA) {
+      notes.push('A');
+    }
+    if (this.chordGenerationIncludeB) {
+      notes.push('B');
+    }
+    if (this.chordGenerationIncludeC) {
+      notes.push('C');
+    }
+    if (this.chordGenerationIncludeD) {
+      notes.push('D');
+    }
+    if (this.chordGenerationIncludeE) {
+      notes.push('E');
+    }
+    if (this.chordGenerationIncludeF) {
+      notes.push('F');
+    }
+    if (this.chordGenerationIncludeG) {
+      notes.push('G');
+    }
+
+    let chordTypes = [];
+
+    if (this.chordGenerationIncludeStandard) {
+      chordTypes.push('');
+    }
+    if (this.chordGenerationIncludeMaj) {
+      chordTypes.push('maj');
+    }
+    if (this.chordGenerationIncludeMin) {
+      chordTypes.push('min');
+    }
+    if (this.chordGenerationInclude7) {
+      chordTypes.push('7');
+    }
+    if (this.chordGenerationIncludeMaj7) {
+      chordTypes.push('maj7');
+    }
+    if (this.chordGenerationIncludeMin7) {
+      chordTypes.push('min7');
+    }
+    if (this.chordGenerationIncludeSus4) {
+      chordTypes.push('sus4');
+    }
+    if (this.chordGenerationIncludeDim) {
+      chordTypes.push('dim');
+    }
+    if (this.chordGenerationIncludeAug) {
+      chordTypes.push('aug');
+    }
+
+
+    const randomNote = notes[Math.floor(Math.random() * notes.length)];
+    const randomChordType = chordTypes[Math.floor(Math.random() * chordTypes.length)];
+
+    return `${randomNote} ${randomChordType}`;
+  }
 }
