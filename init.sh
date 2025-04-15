3dad7a8cc6267b2127b85335f30351555e082fab
Craig Cochrane <craig@cochrane.cc>
2025-04-15 11:30:57 +0100

    Added ALSA install to init.sh

--- init.sh
+++ init.sh
@@ -6,5 +6,6 @@ sudo apt install cmake
 sudo apt install clang-tidy
 sudo apt install nginx
 sudo apt install libwebsockets-dev
+sudo apt install libasound2-dev
 
 echo "Init of Enviroment completed."
