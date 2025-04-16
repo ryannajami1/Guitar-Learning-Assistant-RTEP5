a8e8760e3a8bac5187bc211835d1e44f61b0c0b3
RaphaelNekam <raphael.nekam@aon.at>
2025-04-16 18:22:10 +0100

    websocket interface created

---
+++ websocket/websocket.hpp
@@ -0,0 +1,22 @@
+#ifndef WEBSOCKET_HPP
+#define WEBSOCKET_HPP
+
+#include <libwebsockets.h>
+#include <array>
+
+constexpr int MAX_PAYLOAD_SIZE = 128;
+
+extern int interrupted;
+extern struct lws_context *context;
+extern struct lws *global_wsi;
+extern int counter;
+
+extern const struct lws_protocols kProtocols[];
+
+int CallbackWs(struct lws *wsi, enum lws_callback_reasons reason,
+               void *user, void *incomingMsg, size_t len);
+
+void SendFunction(int num);
+void SignalHandler(int sig);
+
+#endif  // WEBSOCKET_HPP
