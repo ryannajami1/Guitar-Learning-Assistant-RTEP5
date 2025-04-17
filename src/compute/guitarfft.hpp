8fe418b11af5a836b07d40d277c60f193802c1a4
Scott Pinkerton <swpinkerton11@gmail.com>
2025-04-17 14:07:09 +0100

    clang tidy auto fix2

--- src/compute/guitarfft.hpp
+++ src/compute/guitarfft.hpp
@@ -49,9 +49,9 @@ private:
     void AddFrame(std::array<int16_t, FRAMES> &frame);
     void ProcessFrames(std::vector<int16_t> buf);
     [[nodiscard]] auto GetFrequencyPeaks() const -> std::vector<std::pair<float, float>>;
-    void WriteFrequencyDataToFile(const std::string &filename);
-    void PrintFrequencyPeaks();
+    static void WriteFrequencyDataToFile(const std::string &filename);
+    static void PrintFrequencyPeaks();
     [[nodiscard]] auto GetFramesCollected() const -> unsigned int;
     [[nodiscard]] auto GetFramesNeeded() const -> unsigned int;
-    void Cleanup();
+    static void Cleanup();
 };
